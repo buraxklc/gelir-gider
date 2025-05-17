8486630015dcef89e394d8587939e66c5f462398
Burak <134539808+buraxklc@users.noreply.github.com>
2025-05-18 02:46:36 +0300

    Delete gelir-gider-takip directory

--- gelir-gider-takip/cron/process-recurring.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../includes/recurring-functions.php';
-
-// Tekrarlayan işlemleri işle
-processRecurringTransactions();
-
-echo "Recurring transactions processed at " . date('Y-m-d H:i:s') . "\n";
-?>
