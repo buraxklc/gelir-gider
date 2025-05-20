e5ef8f1aab5c69116e60895820654c76b055cda7
Burak <134539808+buraxklc@users.noreply.github.com>
2025-05-20 22:00:37 +0300

    Delete gelir-gider-takip directory

--- gelir-gider-takip/version.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Scriptin mevcut sürüm bilgisi
-define('APP_VERSION', '1.0.5');
-define('APP_NAME', 'Gelir-Gider Takip');
-define('UPDATE_SERVER', 'https://guncelleme.burakkilic.com.tr/updates/');
